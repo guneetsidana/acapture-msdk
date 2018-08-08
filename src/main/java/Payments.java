c267e114e46616b7bace5e3e61377a1e9505fe4b
Divyansh Kumar <divyansh.kumar1@gmail.com>
2018-08-08 16:52:40 +0200

    First Commit Setting Up The Server

---
+++ src/main/java/Payments.java
@@ -0,0 +1,39 @@
+import static spark.Spark.*;
+import spark.utils.IOUtils;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class Payments {
+    public static void main(String[] args) {
+        get("/checkout", (request, response) -> {
+
+            // Get the checkout Id.
+            URL url = new URL("https://test.acaptureservices.com/v1/checkouts");
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            String postParams = "authentication.userId=8a829417572279ad015732d66d0327ba&authentication.password=Ena74K3gzS&authentication.entityId=8a829417572279ad015732d66cb427b6";
+
+            for (String key: request.queryParams()) {
+                postParams = postParams.concat("&"+key+"="+request.queryParams(key));
+            }
+
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+
+            int responseCode = conn.getResponseCode();
+            InputStream is;
+
+            if (responseCode >= 400) is = conn.getErrorStream();
+            else is = conn.getInputStream();
+
+            return IOUtils.toString(is);
+        });
+    }
+}
